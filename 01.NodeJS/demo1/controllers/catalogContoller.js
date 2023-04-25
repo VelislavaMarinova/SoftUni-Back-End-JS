3984157fd2b01e95cb8efa80e8d0c4f8c8e31130
Vel MAr <velmar.annabell@gmail.com>
2023-04-25 12:17:04 +0300

    add dynamic data to catalog

--- 01.NodeJS/demo1/controllers/catalogContoller.js
+++ 01.NodeJS/demo1/controllers/catalogContoller.js
@@ -1,9 +1,10 @@
-const {html}=require("../util");
+const {html,data}=require("../util");
 
 function catalogPage(req, res) {
     res.write(html(`
     <h1>Catalog</h1>
     <p>List of Items</p>
+    <ul>${data.map(x=>`<li>${x.name}-${x.color}</li>`).join('\n')}</ul>
     `, 'Catalog'));
     res.end();
 }
