21cf299f75b6a0cda8497675165d621e2dd79dd0
Vel MAr <velmar.annabell@gmail.com>
2023-04-25 11:49:07 +0300

    add router.js and separeting concerns

--- 01.NodeJS/demo1/index.js
+++ 01.NodeJS/demo1/index.js
@@ -1,34 +1,17 @@
 const http = require('http');
-const { homePage, aboutPage } = require('./controllers/homeContoller');
+const router = require('./router')
+const { homePage, aboutPage, defaultPage } = require('./controllers/homeContoller');
 const { catalogPage } = require('./controllers/catalogContoller');
 
+router.register('/', homePage);
+router.register('/catalog', catalogPage);
+router.register('/about', aboutPage);
+router.register('default', defaultPage);
 
-const routes = {
-    '/': homePage,
-    '/about': aboutPage,
-    '/catalog': catalogPage,
-}
+// const server = http.createServer((req, res) => {
+//     router.match(req, res);
+// }); ===
+const server = http.createServer(router.match)
 
-const server = http.createServer((req, res) => {
-    console.log('Request sent');
-    // console.log(request);
-    console.log(req.method);
-    console.log(req.headers);
-    console.log(req.url);
-
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    console.log(url);
-
-    const handler = routes[url.pathname];
-
-    if (typeof handler == 'function') {
-        handler(req, res);
-        // response.write(html(page));
-        // response.end();
-    } else {
-        defaultPage(req, res);
-    }
-
-});
 server.listen(3000);
 
