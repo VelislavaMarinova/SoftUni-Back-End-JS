0c5be3bb38bc5bc8fe126323449d738b6c576838
Vel MAr <velmar.annabell@gmail.com>
2023-04-25 17:24:38 +0300

    use formidable and constructor  IncomingForm, post request

--- 01.NodeJS/demo1/index.js
+++ 01.NodeJS/demo1/index.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const router = require('./router')
 const { homePage, aboutPage, defaultPage } = require('./controllers/homeContoller');
-const { catalogPage, createPage } = require('./controllers/catalogContoller');
+const { catalogPage, createPage, createItem } = require('./controllers/catalogContoller');
 
-router.register('/', homePage);
-router.register('/catalog', catalogPage);
-router.register('/create', createPage);
-router.register('/about', aboutPage);
-router.register('default', defaultPage);
+router.get('/', homePage);
+router.get('/catalog', catalogPage);
+router.get('/create', createPage);
+router.post('/create', createItem);
+router.get('/about', aboutPage);
+router.get('default', defaultPage);
+
 
 // const server = http.createServer((req, res) => {
 //     router.match(req, res);
--- 01.NodeJS/demo1/router.js
+++ 01.NodeJS/demo1/router.js
@@ -1,22 +1,33 @@
 const routes = {};
 
-function register(path, handler) {
-    routes[path] = handler;
+function register(method, path, handler) {
+    if (routes[path] == undefined) {
+        routes[path] = {};
+    }
+    routes[path][method] = handler;
 }
 
 function match(req, res) {
     const url = new URL(req.url, `http://${req.headers.host}`);
 
-    const handler = routes[url.pathname];
-
+    const actions = routes[url.pathname];
+    let handler;
+    if (actions != undefined) {
+        handler = actions[req.method];
+    }
     if (typeof handler == "function") {
         handler(req, res);
     } else {
-        routes.default(req, res)
+        routes.default['GET'](req, res)
     }
+
+    // const handler = routes[url.pathname][rec.method];
+
 }
 
 module.exports = {
     register,
+    get:register.bind(null,"GET"),
+    post: register.bind(null,"POST"),
     match
 }
