8fcb7b254c163d942901eb2f01b206cc3ded9590
Vel MAr <velmar.annabell@gmail.com>
2023-04-26 13:36:30 +0300

    create infopage

--- 01.NodeJS/cat-shelter/resources/index.js
+++ 01.NodeJS/cat-shelter/resources/index.js
@@ -1,30 +1,38 @@
 const http = require('http');
 
-const css = require('./css/site.css')
-const homePage = require('./views/index')
+const css = require('./css/site.css');
+const homePage = require('./views/index');
+const infoCat = require('./views/infoCat');
+const cats = require('./cats.json')
 
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200,{
-    'Content-Type': 'text/html'
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
     })
- 
-    switch (req.url) {
-        case '/':
-            res.write(homePage);
-            break;
-        case '/css/site.css': 
-        res.writeHead(200,{
-            'Content-Type': 'text/css'
-            })
-        res.write(css)
 
-        default: res.write(`
-        <h1>404</h1>
-        `)
-            break;
+
+
+    if (req.url == '/') {
+        res.write(homePage);
+
+    } else if (/cats\/\d+\/edit/.test(req.url)) {
+        let catId = req.url.split('/')[2];
+        let cat = cats.find(x => x.id == catId);
+        res.write(infoCat(cat));
     }
+    else if (req.url == '/css/site.css') {
 
+        res.writeHead(200, {
+            'Content-Type': 'text/css'
+        })
+        res.write(css)
+    } else {
+
+        res.write(`
+                <h1>404</h1>
+                `)
+    }
     res.end();
 });
 
