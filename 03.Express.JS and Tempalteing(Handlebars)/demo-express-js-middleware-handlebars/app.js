6c103db570ef87859ea84d9c69c87f5e0cfe0e03
Vel MAr <velmar.annabell@gmail.com>
2023-05-11 13:27:18 +0300

    templating

--- 03.Express.JS and Tempalteing(Handlebars)/demo-express-js-middleware-handlebars/app.js
+++ 03.Express.JS and Tempalteing(Handlebars)/demo-express-js-middleware-handlebars/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const hbs= require('express-handlebars');
+const hbs = require('express-handlebars');
 const app = express();
 
 
-app.engine('hbs',hbs.engine());
-app.set('view engine','hbs');
+app.engine('hbs', hbs.engine());
+app.set('view engine', 'hbs');
 
 // app.get('/', (req, res) => {
 //     res.send(`<h1>Hello from Express</h1>
@@ -14,26 +14,66 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 app.get('/cats', (req, res) => {
-    res.render('cats');
+    const cats=[
+    {
+        id: 1,
+        name: "Bobi",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    },
+    {
+        id: 2,
+        name: "Tomi",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    },
+    {
+        id: 3,
+        name: "Jerry",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    },
+    {
+        id: 4,
+        name: "Kitie",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    },
+    {
+        id: 5,
+        name: "Domi",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    },
+    {
+        id: 6,
+        name: "Roni",
+        breed: "Bombay Cat",
+        description: "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho."
+    }
+]
+    res.render('cats',{cats});
+  
 });
+
 
 // app.get('/cats', (req, res) => {
 //     res.send(`<h1>Cats Page</hq>`)
 // });
 
 let validateCatId = (req, res, next) => {
-    let catId = Number(req.params.catID);
+    let catId = Number(req.params.catId);
     if (!catId) {
-       return res.send('Invalid Id');
+        return res.send('Invalid Id');
     } else {
         next()
-    }};
+    }
+};
 
-    //middleware
-    app.get('/cats/:catID',validateCatId, (req, res) => {
-    console.log(req.params);
-    let catId = req.params
-    res.send(`<h1>One cat ${req.params.catID}</h1>`);
+//middleware
+app.get('/cats/:catId', validateCatId, (req, res) => {
+    // res.send(`<h1>One cat ${req.params.catID}</h1>`);
+    res.render('cat', { id: req.params.catId })
 });
 
 app.get('/dogs', (req, res) => {
