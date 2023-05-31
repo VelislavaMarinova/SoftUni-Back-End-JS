1596c06237b286b1b208f5a95d581f61f879e8a9
Vel MAr <velmar.annabell@gmail.com>
2023-05-31 17:32:57 +0300

    search

--- 09.Exam prep 2nd part/crypto-trade/models/CryptoOffer.js
+++ 09.Exam prep 2nd part/crypto-trade/models/CryptoOffer.js
@@ -3,18 +3,27 @@ const mongoose = require('mongoose');
 const cryptoOfferSchema = new mongoose.Schema({
     name: {
         type: String,
+        minLength: 2,
         required: true,
     },
     image: {
         type: String,
         required: true,
+        validate:{
+            validator: function(value){
+                return value.startsWith('http://')||value.startsWith('https://')
+            },
+            message: 'URL is invalid!'
+        }
     },
     price: {
         type: Number,
+        min: 0,
         required: true,
     },
     description: {
         type: String,
+        minLength: 10,
         required: true,
     },
     paymentMethod: {
