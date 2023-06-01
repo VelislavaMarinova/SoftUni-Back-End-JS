04c5193f425e69bfabf10627802acea65406fcc5
Vel MAr <velmar.annabell@gmail.com>
2023-06-02 00:04:40 +0300

    create bookReview

--- Expams/Book Talk/controllers/bookController.js
+++ Expams/Book Talk/controllers/bookController.js
@@ -2,13 +2,44 @@ const router = require('express').Router();
 
 const { isAutorized } = require('../middlewares/authMW');// for routeguard
 
-router.get('/create', isAutorized, (req, res) => {//isAutorized routeguard
-    res.render('sth/create');
+const bookService = require('../services/bookService');
+const { getErrorMessaage } = require('../utils/errorUtils');
+
+
+router.get('/catalog',(req,res)=>{
+    res.render('book/catalog')
+})
+
+router.get('/create', isAutorized, async (req, res) => {//isAutorized routeguard
+    res.render('book/create');
+});
+router.post('/create', isAutorized, async (req, res) => {//isAutorized routeguard
+
+    const bookReviewData = req.body;
+    const ownerId = req.user._id;
+
+    try {
+
+        await bookService.create(ownerId, bookReviewData);
+
+    } catch (error) {
+        return res.status(400).render('book/create', { error: getErrorMessaage(error) });
+    }
+    res.redirect('/book/catalog');
 });
 
-router.post('/create', isAutorized, (req, res) => {//isAutorized routeguard
+// router.get('/catalog', async (req, res) => {
 
-});
+//     try {
+//         const allCryptoOffers = await cryptoService.getAll().lean();
+//         res.render('crypto/catalog', { allCryptoOffers });
+
+//     } catch (error) {
+
+//         return res.status(400).render('crypto/catalog', { error: getErrorMessaage(error) });
+
+//     }
+// });
 
 
 module.exports = router;
